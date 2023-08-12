8e61ba3777377d4f246794f23fafd2d130cf7e11
user1 <user1.gmail.com>
2023-08-12 22:13:48 +0530

    akshat's commit

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
- 
+
 require("dotenv").config({ path: "backend/config/config.env" });
 app.use(cors());
-app.use(express.json({ limit: "1000mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //  app.use(cookieParser());
 
 const tanishk = require("./routes/registration");
 const goyal = require("./routes/login"); //NEW
-const akshat=require("./routes/question_route");
-const sharma=require("./routes/evaluate");
+const akshat = require("./routes/question_route");
+const sharma = require("./routes/evaluate");
 app.use("/register", tanishk);
 app.use("/login", goyal); //NEW
-app.use("/question",akshat);
-app.use("/evaluate",sharma);
+app.use("/question", akshat);
+app.use("/evaluate", sharma);
 
 module.exports = app;
